4449407635207672b5aa2f9ba5b7d4b62a1d4bc5
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-01-20 23:53:03 -0500

    colors

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -21,15 +21,12 @@ private:
 
   bool on_key_press(GdkEventKey* e);
 
-  // Scaling
   void on_overlay_size_allocate(Gtk::Allocation& alloc);
   void apply_ui_scale(int w, int h);
 
-  // Swipe/drag
   void setup_gestures();
   void handle_swipe_delta(double dx, double dy, guint32 dt_ms);
 
-  // Layout
   Gtk::Overlay overlay_;
   Gtk::Box     root_{Gtk::ORIENTATION_HORIZONTAL};
 
@@ -41,7 +38,6 @@ private:
   Desktop* page1_ = nullptr;
   Desktop* page2_ = nullptr;
 
-  // Bottom-left scheme buttons
   Gtk::Box     scheme_bar_{Gtk::ORIENTATION_HORIZONTAL};
   Gtk::Button  scheme_day_;
   Gtk::Button  scheme_dusk_;
@@ -51,16 +47,14 @@ private:
 
   Scheme scheme_ = Scheme::Day;
 
-  // IMPORTANT: force first scale apply
+  // Force first apply
   double ui_scale_ = -1.0;
   bool show_labels_ = true;
 
-  // Gesture state
   Glib::RefPtr<Gtk::GestureDrag> drag_;
   bool   drag_claimed_ = false;
   gint64 drag_t0_us_ = 0;
 
-  // thresholds
   static constexpr double  kSwipeLockPx      = 18.0;
   static constexpr double  kSwipeMinPx       = 120.0;
   static constexpr double  kSwipeFastMinPx   = 70.0;
